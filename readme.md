42abe33eda37bb63276fe32a96e66329cda00dc5
Matt Folkard <matt.folkard@matmi.com>
2018-10-29 19:25:49 +0000

    Added readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+# FrothHiss
+
+## Installation
+
+- `npm install`
+
+## Commands
+
+- `npm start` - run the program, outputs to console
+- `npm test` - start mocha test runner for a single run
+- `npm run test:watch` - start mocha test runner and constantly listen for changes to source files
--- test/frothhiss.spec.js
+++ test/frothhiss.spec.js
@@ -28,25 +28,28 @@ describe('frothhiss', () => {
     it('should return a list of the correct size', () => {
         expect(frothhiss.run(1, 5).length).to.equal(5);
     });
+
     it('should return the appropriate results for 1-15', () => {
         let output = frothhiss.run(1, 15);
-        expect(output).to.deep.equal(testData);
+        expect(output).to.equal(testData);
     });
+
     describe('getResult', () => {
         it('should return 1 for 1', () => {
-            expect(frothhiss.getResult(1)).to.deep.equal('1');
+            expect(frothhiss.getResult(1)).to.equal('1');
         });
 
         it('should return Froth for multiples of 3', () => {
             expect(frothhiss.getResult(3)).to.equal('Froth');
             expect(frothhiss.getResult(6)).to.equal('Froth');
             expect(frothhiss.getResult(9)).to.equal('Froth');
+            expect(frothhiss.getResult(15)).to.contain('Froth');
         });
 
         it('should return Hiss for multiples of 5', () => {
             expect(frothhiss.getResult(5)).to.equal('Hiss');
             expect(frothhiss.getResult(10)).to.equal('Hiss');
-            expect(frothhiss.getResult(15)).to.not.equal('Hiss');
+            expect(frothhiss.getResult(15)).to.contain('Hiss');
         });
 
         it('should return FrothHiss when inout is 15', () => {
